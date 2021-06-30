88bc76c6fa775ba60f51a48cbff513eade4d5b91
honghogn <84121863+honghogn@users.noreply.github.com>
2021-06-30 13:48:56 +0900

    Add files via upload

--- day100/Header.jsx
+++ day100/Header.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const  Header= () => {
     return (
-        <>
+        <div>
           <h1 style={{ color:'black',
                         backgroundColor:'white'
             }}> 
             허홍주 </h1>  
-        </>
+        </div>
     );
 };
 
